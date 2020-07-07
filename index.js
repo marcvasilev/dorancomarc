c8a96721c444bdd2387613f297c3b8cadf39c403
“Doranco <mladenatanasov75@gmail.com>
2020-07-07 14:46:17 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+//function sum( a,  b){
+	//return a + b;
+//}
+//exercice 1 
+
+for(var i=50; i<=65; i++){
+	//console.log(i)
+}
+
+
+for(var i=1;i<50;i++){
+	if(i%3 ===0 || i % 7 ===0){
+		//console.log(i)
+	}
+}
+
+for(var i=1;i<150;i++){
+	if(i%5 ===0 && i % 7 ===0){
+		//console.log(i)
+	}
+}
+
+var a = 0;
+for(var i=10;i<=20;i++){
+	a+= i;
+	
+}
+console.log(a)
+
+
+
+//module.exports = sum;
